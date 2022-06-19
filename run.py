55f47b67f767a157b790fb941f9ba267f6ddcb1f
Morten W. Petersen <morphex@gmail.com>
2022-06-19 20:44:56 +0200

    Adding support for CSV files with semicolon separator. Refactoring debug code. Adding support for conversion of RVN*USD value into local currency

--- run.py
+++ run.py
@@ -11,6 +11,11 @@ DATA_FILE = ""
 ZERO = Decimal(0)
 DEBUG = True
 
+def DEBUG_PRINT(*arguments):
+    if DEBUG:
+        print(*arguments, file=sys.stderr)
+
+
 DATA_FILE = sys.argv[1]
 
 year = None
@@ -31,6 +36,20 @@ try:
     usd_exchange_config = sys.argv[4]
 except IndexError:
     pass
+
+def guess_separator(csv_data_lines, default=","):
+    """Returns a guess at what the column separator is in the CSV data."""
+    semicolon_count_header = csv_data_lines[0].count(";")
+    semicolon_count_data = semicolon_count_header
+    comma_count_header = csv_data_lines[0].count(",")
+    comma_count_data = comma_count_header
+    for line in csv_data_lines[1:]:
+        semicolon_count_data += line.count(";")
+        comma_count_data += line.count(",")
+    if comma_count_data > semicolon_count_data:
+        return ","
+    if comma_count_data < semicolon_count_data:
+        return ";"
 
 def guess_date_format(dates):
     """Guess the date format given a set of dates."""
@@ -76,6 +95,13 @@ def parse_date(date, format):
     year, month, day = date_[format[0]], date_[format[1]], date_[format[2]]
     return int(year), int(month), int(day)
 
+def parse_float(value):
+    """Parses a float, using either a . or , as the denominator."""
+    try:
+        return float(value)
+    except ValueError:
+        return float(value.replace(",", "."))
+
 def parse_rate_file(filename, date, low, high, skip_header=True):
     rates = OrderedDict()
     lines = open(filename).readlines()[1:]
@@ -84,18 +110,18 @@ def parse_rate_file(filename, date, low, high, skip_header=True):
     high_index = int(high)
     date_format = ""
     dates = []
+    separator = guess_separator(lines)
     for line in lines:
-        dates.append(line.split(",")[date_index])
+        dates.append(line.split(separator)[date_index])
     date_format = guess_date_format(dates)
     year_index, month_index, day_index = date_format
-    if DEBUG:
-        print("Date format", year_index, month_index, day_index, file=sys.stderr)
-        print("First date", parse_date(lines[0].split(",")[date_index], date_format), file=sys.stderr)
+    DEBUG_PRINT("Date format", year_index, month_index, day_index)
+    DEBUG_PRINT("First date", parse_date(lines[0].split(separator)[date_index], date_format))
     for line in lines:
-        line_ = line.split(",")
+        line_ = line.split(separator)
         date = parse_date(line_[date_index], date_format)
-        low_ = Decimal(line_[low_index])
-        high_ = Decimal(line_[high_index])
+        low_ = Decimal(parse_float(line_[low_index]))
+        high_ = Decimal(parse_float(line_[high_index]))
         rates[date] = ((low_ + high_) / 2)
     return rates
 
@@ -108,19 +134,57 @@ if usd_rate_config:
         print("Rate config: file name,date index,low rate index,high rate index")
         sys.exit(1)
     usd_rates = parse_rate_file(usd_rate_file, date, low, high)
-    if DEBUG:
-        print(list(usd_rates.items())[0], file=sys.stderr)
+    DEBUG_PRINT(list(usd_rates.items())[0])
+
+def parse_exchange_file(filename, date, low, high, skip_header=True):
+    rates = OrderedDict()
+    lines = open(filename).readlines()[1:]
+    date_index = int(date)
+    low_index = int(low)
+    high_index = int(high)
+    date_format = ""
+    dates = []
+    separator = guess_separator(lines)
+    for line in lines:
+        dates.append(line.split(separator)[date_index])
+    date_format = guess_date_format(dates)
+    year_index, month_index, day_index = date_format
+    DEBUG_PRINT("Exchange rate date format", year_index, month_index, day_index)
+    DEBUG_PRINT("Exchange rate first date", parse_date(lines[0].split(separator)[date_index], date_format))
+    for line in lines:
+        line_ = line.split(separator)
+        DEBUG_PRINT("line, date_index:", line_, date_index)
+        date = parse_date(line_[date_index], date_format)
+        low_ = Decimal(parse_float(line_[low_index]))
+        high_ = Decimal(parse_float(line_[high_index]))
+        rates[date] = ((low_ + high_) / 2)
+    return rates
+
+exchange_rates = {}
+
+if usd_exchange_config:
+    try:
+        usd_exchange_file, date, low, high = usd_exchange_config.split(",")
+    except ValueError:
+        try:
+            usd_exchange_file, date, rate = usd_exchange_config.split(",")
+            low = high = rate
+        except ValueError:
+            print("USD Exchange rate config: file name,date index,low rate index,high rate index")
+            print("Or: file name,date index,exchange rate index")
+            sys.exit(1)
+    exchange_rates = parse_exchange_file(usd_exchange_file, date, low, high)
+    DEBUG_PRINT(list(exchange_rates.items())[0])
 
 def lookup_rate(rate_database, date):
     """Looks up a rate for a given date.  If one doesn't exist for that day,
     return the rate of the first date before the given date."""
     try:
-        return rate_database[date]
+        return Decimal(rate_database[date])
     except KeyError:
         date_ = Date(*date)
         date_ = date_ - timedelta(days=1)
-        if DEBUG:
-            print("Date", date, "not found, looking at", date_, file=sys.stderr)
+        DEBUG_PRINT("Date", date, "not found, looking at", date_)
         return lookup_rate(rate_database, (date_.year, date_.month, date_.day))
 
 balance = Decimal(0.0)
@@ -133,13 +197,14 @@ additional_headers = ["Balance"]
 if usd_rate_config:
     additional_headers.extend(["USD rate", "USD converted"])
 if usd_exchange_config:
-    additiona._headers.append("Local currency")
+    additional_headers.append("Exchange rate for LC")
+    additional_headers.append("Local currency")
 print(",".join(header),"," + ",".join(additional_headers),sep="")
 dates = []
 for line in data:
     dates.append(line[0])
 date_format = guess_date_format(dates)
-print("RVN Data date format:", date_format, file=sys.stderr)
+DEBUG_PRINT("RVN Data date format:", date_format)
 for line in data:
     date = parse_date(line[0], date_format)
     balance += Decimal(line[-1])
@@ -153,11 +218,19 @@ for line in data:
     if printable:
         if usd_rates:
             try:
-                total = Decimal(line[-1]) * lookup_rate(usd_rates, date)
-                if DEBUG:
-                    print((total, str(total)), file=sys.stderr)
-                additional_data.extend([str(lookup_rate(usd_rates, date)), str(total)])
+                exchange_rate =  lookup_rate(usd_rates, date)
+                total = Decimal(line[-1]) * exchange_rate
+                DEBUG_PRINT((total, str(total)))
+                additional_data.extend([str(exchange_rate), str(total)])
             except KeyError:
                 additional_data.extend(["", ""])
+            if exchange_rates:
+                try:
+                    exchange_rate = lookup_rate(exchange_rates, date)
+                    total2 = total * exchange_rate
+                    DEBUG_PRINT((exchange_rate, total2, str(total2)))
+                    additional_data.extend([str(exchange_rate), str(total2)])
+                except KeyError:
+                    additional_data.extend([""])
     if printable:
         print(','.join(line),",",",".join(additional_data),sep="")

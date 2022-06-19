e08301ba0d0f2cd522d5aa82924a1f42f78af905
Morten W. Petersen <morphex@gmail.com>
2022-06-19 20:45:18 +0200

    Adding another test, to illustrate the command line

--- test.sh
+++ test.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-./run.py tests/randomly_selected_transactions.csv 2021 
+./run.py tests/randomly_selected_transactions.csv 2021
+# RVN-USD from Yahoo data service RVN->USD
+# USD-NOK format from Norwegian central bank data service USD->NOK
+./run.py tests/randomly_selected_transactions.csv 2021 ../RVN-USD.csv,0,3,2 ../USD-NOK.csv,14,15 1> processed_transactions_2021.csv
